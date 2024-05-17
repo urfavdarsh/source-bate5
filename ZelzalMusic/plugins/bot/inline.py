246a3ac97c51ba057b3183a720eeaaea08412ddd
urfavdarsh <151478412+urfavdarsh@users.noreply.github.com>
2024-05-17 17:48:49 +0300

    Add files via upload

---
+++ ZelzalMusic/plugins/bot/inline.py
@@ -0,0 +1,69 @@
+
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultPhoto,
+)
+from youtubesearchpython.__future__ import VideosSearch
+
+from ZelzalMusic import app
+from ZelzalMusic.utils.inlinequery import answer
+from config import BANNED_USERS
+
+
+@app.on_inline_query(~BANNED_USERS)
+async def inline_query_handler(client, query):
+    text = query.query.strip().lower()
+    answers = []
+    if text.strip() == "":
+        try:
+            await client.answer_inline_query(query.id, results=answer, cache_time=10)
+        except:
+            return
+    else:
+        a = VideosSearch(text, limit=20)
+        result = (await a.next()).get("result")
+        for x in range(15):
+            title = (result[x]["title"]).title()
+            duration = result[x]["duration"]
+            views = result[x]["viewCount"]["short"]
+            thumbnail = result[x]["thumbnails"][0]["url"].split("?")[0]
+            channellink = result[x]["channel"]["link"]
+            channel = result[x]["channel"]["name"]
+            link = result[x]["link"]
+            published = result[x]["publishedTime"]
+            description = f"{views} | {duration} دقيقـة | {channel}  | {published}"
+            buttons = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="يوتيوب",
+                            url=link,
+                        )
+                    ],
+                ]
+            )
+            searched_text = f"""
+❄ <b>العنـوان :</b> <a href={link}>{title}</a>
+
+⏳ <b>مدة التشـغيل :</b> {duration} دقيقـة
+👀 <b>المشاهـدات :</b> <code>{views}</code>
+🎥 <b>القنـاة :</b> <a href={channellink}>{channel}</a>
+⏰ <b>بواسطـة :</b> {published}
+
+
+<u><b>⎉︙تم البحث اونلايـن بواسطـة {app.name}</b></u>"""
+            answers.append(
+                InlineQueryResultPhoto(
+                    photo_url=thumbnail,
+                    title=title,
+                    thumb_url=thumbnail,
+                    description=description,
+                    caption=searched_text,
+                    reply_markup=buttons,
+                )
+            )
+        try:
+            return await client.answer_inline_query(query.id, results=answers)
+        except:
+            return
