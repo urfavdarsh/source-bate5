5121f5870ca61ade0cbbb27b01b11d840007acf8
urfavdarsh <151478412+urfavdarsh@users.noreply.github.com>
2024-05-17 20:26:15 +0300

    Update __main__.py

--- ZelzalMusic/__main__.py
+++ ZelzalMusic/__main__.py
@@ -37,26 +37,26 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("ZelzalMusic.plugins" + all_module)
-    LOGGER("ميــوزك دارك").info("تم تحميل الاضافات ...✓")
+    LOGGER("ميــوزك بطيخ").info("تم تحميل الاضافات ...✓")
     await userbot.start()
     await Zelzaly.start()
     try:
         await Zelzaly.stream_call("https://telegra.ph/file/b3e5093cb07c6185cd3fc.jpg")
     except NoActiveGroupCall:
-        LOGGER("ميــوزك دارك").info(
+        LOGGER("ميــوزك بطيخ").info(
             "خطأ .. قم بفتح المكالمة في مجموعة السجل الخاصه بك\n\nجارِ ايقاف بوت الميوزك . . ."
         )
         exit()
     except:
         pass
     await Zelzaly.decorators()
-    LOGGER("ميــوزك دارك").info(
+    LOGGER("ميــوزك بطيخ").info(
         "\x44\x6f\x6e\x65\x20\x69\x6e\x73\x74\x61\x6c\x6c\x61\x74\x69\x6f\x6e\x20\x5a\x54\x68\x6f\x6e\x20\x4d\x75\x73\x69\x63\x20\xe2\x9c\x93\x0a\x47\x6f\x20\x74\x6f\x20\x79\x6f\x75\x72\x20\x62\x6f\x74\x20\x61\x6e\x64\x20\x73\x65\x6e\x64\x20\x2f\x73\x74\x61\x72\x74\x0a\x43\x68\x61\x6e\x6e\x65\x6c\x20\x3a\x20\x28\x40\x5a\x54\x68\x6f\x6e\x29"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("ميــوزك دارك").info("جارِ ايقاف بوت الميوزك . . .")
+    LOGGER("ميــوزك بطيخ").info("جارِ ايقاف بوت الميوزك . . .")
 
 
 if __name__ == "__main__":
