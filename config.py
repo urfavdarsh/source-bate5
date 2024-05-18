abac258bcd5c578ea6a128a2f1cfbe4a04b4979d
urfavdarsh <151478412+urfavdarsh@users.noreply.github.com>
2024-05-18 10:52:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sir-ahmed/DEV.git",
+    "https://github.com/urfavdarsh/source-bate5",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
